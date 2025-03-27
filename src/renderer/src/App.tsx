d2a5ffd137dcd3477e25daddcaad922f945c248a
Yichen Yue <yue.yichen1992@gmail.com>
2025-03-27 01:18:31 -0500

    make the divider adjustable

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,24 +1,27 @@
+import { Panel, PanelGroup, PanelResizeHandle } from 'react-resizable-panels'
 import { Textarea } from './components/ui/textarea'
 import { FileDialog } from './features/file-explorer/components/FileDialog'
-// import { Textarea } from '@/components/ui/textarea'
 
 function App(): JSX.Element {
   return (
     <div className="flex h-screen bg-background text-foreground">
-      <div className="w-1/3 flex flex-col border-r border-border">
-        <FileDialog />
-      </div>
+      <PanelGroup direction="horizontal">
+        <Panel minSize={35} defaultSize={40} className="flex flex-col">
+          <FileDialog />
+        </Panel>
 
-      {/* Right Panel (takes remaining width) - Make it a flex column */}
-      <div className="flex-grow p-6 flex flex-col gap-4">
-        {' '}
-        <h2 className="text-xl font-semibold">Content Area</h2>
-        {/* Textarea - Use flex-grow to make it fill available vertical space */}
-        <Textarea
-          placeholder="Enter text here..."
-          className="flex-grow resize-none" // flex-grow makes it expand, resize-none disables manual resizing handle
-        />
-      </div>
+        <PanelResizeHandle className="w-1 border-l border-r border-border cursor-col-resize" />
+
+        {/* Right Panel (takes remaining width) - Make it a flex column */}
+        <Panel minSize={20} className="p-6 flex flex-col gap-4">
+          <h2 className="text-xl font-semibold">Content Area</h2>
+          {/* Textarea - Use flex-grow to make it fill available vertical space */}
+          <Textarea
+            placeholder="Enter text here..."
+            className="flex-grow resize-none" // flex-grow makes it expand, resize-none disables manual resizing handle
+          />
+        </Panel>
+      </PanelGroup>
     </div>
   )
 }
