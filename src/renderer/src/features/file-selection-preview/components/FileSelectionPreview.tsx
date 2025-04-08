4b9b00f482a820d19a06e0a73be3f87d1f6eeb22
Yichen Yue <yue.yichen1992@gmail.com>
2025-04-08 03:29:13 -0500

    save progress on PreviewGroup

--- src/renderer/src/features/file-selection-preview/components/FileSelectionPreview.tsx
+++ src/renderer/src/features/file-selection-preview/components/FileSelectionPreview.tsx
@@ -1,14 +1,18 @@
-import { Textarea } from '@renderer/components/ui/textarea'
 import { Fragment } from 'react/jsx-runtime'
+import { PreviewGroup } from './PreviewGroup'
 
 export function FileSelectionPreview(): JSX.Element {
   return (
     <Fragment>
       <h2 className="text-xl font-semibold">Selected Files</h2>
-      <Textarea
-        placeholder="Enter text here..."
-        className="flex-grow resize-none" // flex-grow makes it expand, resize-none disables manual resizing handle
-      />
+      <PreviewGroup
+        directoryPath="test/aste/asts/a/at"
+        files={[
+          { name: 'test.ts', tokenSize: 280 },
+          { name: 'test2.ts', tokenSize: 280 },
+          { name: 'test3.ts', tokenSize: 280 }
+        ]}
+      ></PreviewGroup>
     </Fragment>
   )
 }
--- src/renderer/src/features/file-selection-preview/components/PreviewGroup.tsx
+++ src/renderer/src/features/file-selection-preview/components/PreviewGroup.tsx
@@ -1,8 +1,44 @@
+import { File, Folder, Trash } from 'lucide-react'
+
 interface PreviewGroupProps {
-  path: string
-  filename: string
+  directoryPath: string
+  files: PreviewFile[]
 }
 
-export function PreviewGroup({ path, filename }: PreviewGroupProps) {
-  return <div></div>
+interface PreviewFile {
+  name: string
+  tokenSize: number
+}
+
+export function PreviewGroup({ directoryPath, files }: PreviewGroupProps) {
+  return (
+    <div className="flex flex-col gap-1.5">
+      <div className="bg-blue-500/30 flex flex-row items-center rounded-sm px-2">
+        <Folder size={16} className="mr-2 text-blue-500" />
+        <p>{directoryPath}</p>
+      </div>
+      <div className="flex flex-wrap gap-2">
+        {files.map((file) => (
+          <div
+            key={file.name}
+            className="w-60 p-2 inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-all disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive border bg-background shadow-xs hover:bg-accent hover:text-accent-foreground dark:bg-input/30 dark:border-input dark:hover:bg-input/50 group"
+          >
+            <div className="flex-grow flex flex-col items-start">
+              <div className="flex flex-row items-center">
+                <File size={16} className="mr-2 text-gray-500" />
+                <p>{file.name}</p>
+              </div>
+              <p>{file.tokenSize}</p>
+            </div>
+            <div
+              onClick={() => {}}
+              className="opacity-0 group-hover:opacity-100 transition-opacity"
+            >
+              <Trash size={16} className="text-red-500" />
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  )
 }
